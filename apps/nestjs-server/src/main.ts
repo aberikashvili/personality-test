dabbb0ff2e571c97a8b210e53c00a48eb3a3dbc1
Arkadi Berikashvili <arkadi.berikashvili@valor-software.com>
2022-12-10 02:53:11 +0400

    add admin project and shared lib

--- apps/nestjs-server/src/main.ts
+++ apps/nestjs-server/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('PersonaliTy Test')
+    .setDescription('The personality test API description')
+    .setVersion('1.0')
+    .addTag('personality-test')
+    .build();
+  const swagerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger-ui', app, swagerDocument);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
