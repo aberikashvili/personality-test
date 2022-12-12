8956b812b4b776fec41de77c4f49259130bb3853
Arkadi Berikashvili <arkadi.berikashvili@valor-software.com>
2022-12-12 15:41:17 +0400

    save progress

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+
+const MATERIAL_MODULES = [MatButtonModule];
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ...MATERIAL_MODULES],
+  exports: [...MATERIAL_MODULES],
 })
 export class SharedModule {}
